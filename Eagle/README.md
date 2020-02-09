534741519c6448cee9ac5affcdf689a4349caacd
luni64 <lutz.niggl@lunoptics.com>
2020-02-09 17:46:49 +0100

    Added encoder multiplexer test board

---
+++ Eagle/README.md
@@ -0,0 +1,3 @@
+## Experimental Encoder Multiplexer
+
+Multiplexer for 8 encoders with switches. The board can be daisy chained to handle more encoders.
--- src/pollEncoder.h
+++ src/pollEncoder.h
@@ -9,18 +9,20 @@ namespace PollEncoder
 {
     class Encoder
     {
-    public:
+     public:
         Encoder(int _pinA, int _pinB, int mode = INPUT) : value(0), pinA(_pinA), pinB(_pinB)
         {
             pinMode(_pinA, mode);
             pinMode(_pinB, mode);
         }
 
-        volatile std::atomic<int> value;   // use atomic since tick might be called by a timer ISR -> prevent issues
+        volatile std::atomic<int> value; // use atomic since tick might be called by a timer ISR -> prevent issues
 
         void tick();
 
-    protected:
+        int getValue() { return value / 2; }
+
+     protected:
         const int pinA, pinB;
         unsigned last;
     };
